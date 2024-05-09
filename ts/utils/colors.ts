b194d1ef4ea78aa3f6138df451953a3feb3a9ef9
BasAMols <basmols98@gmail.com>
2024-05-10 01:08:57 +0200

    RGBA class for future

--- ts/utils/colors.ts
+++ ts/utils/colors.ts
@@ -9,3 +9,21 @@ export class Colors {
     public static m:Color = [1.0, 0.0, 1.0, 1.0];
     public static w:Color = [1.0, 1.0, 1.0, 1.0];
 }
+export class rgba{
+    constructor(public color: Color) {}
+    get r(){
+        return this.color[0];
+    }
+    get g(){
+        return this.color[1];
+    }
+    get b(){
+        return this.color[2];
+    }
+    get a(){
+        return this.color[3];
+    }
+    get rgb(){
+        return [this.color[1], this.color[2], this.color[3]];
+    }
+}
